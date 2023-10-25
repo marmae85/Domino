77b779acd8b78cde32809b41ac1931b00f4d3d6b
florent <flobirmbout@outlook.fr>
2023-10-25 11:32:10 +0200

    affichage domino

--- Domino.py
+++ Domino.py
@@ -1,30 +1,57 @@
+"""
+- creer domino "A", argument: le tableau de valeurs :                D= Domino([7,8])
+- établir la direction du domino (N,S,E,O) :                         D.setDirection("N")
+- obtenir les valeurs du domino : renvoie un tableau à 2 valeurs     D.getValue()
+- définir l'affichage, en parametre la direction :                   D.setAffichage(D.getDirection)
+  (appelé automatiquement)
+- obtenir l'affichage du domino (renvoie un string                   D.getAffichage()
+"""
+
 class Domino :
     value=[0,0]
-    direction = "E"
+    direction = ""
+    affichage=[str(value[0]),str(value[1])]
 
     def __init__(self,type):
-        self.value = [type[0],type[1]]
+        self.value = type
+        self.direction="E"
 
     def setValue(self,value):
         self.value=value
 
     #  changer le sens ----- A FAIRE-----
-    def set_direction(self,dir):
-        tempo
-        if dir == "N":
-            self.value = value
-        if dir == "S":
-            self.value = value
-        if dir == "E":
-            self.value = value
-        if dir == "O":
-            self.value = value
+    def setDirection(self,dir):
+        self.direction=dir
 
-    def getValue(self,value):
-        self.value=value
+    def getValue(self):
+        print(self.value)
+        return self.value
 
     def getDirection(self,direction):
-        self.direction = direction
+        print(self.direction)
+        return(self.direction)
 
+    def setAffichage(self):
+        self.setAffichage(self.direction)
+        return self.affichage
+
+    def setAffichage(self,dir):
+        v1=str(self.value[0])
+        v2=str(self.value[1])
+        if self.direction == "N":
+            self.affichage = ["|", v1, "|\n|", v2, "|"]
+            self.affichage = "".join(self.affichage)
+
+        elif self.direction == "S":
+            self.affichage = ["|", v2, "|\n|", v3, "|"]
+            self.affichage = "".join(self.affichage)
+
+        elif self.direction == "E":
+            self.affichage = ["|", v1, "|", v2, "|"]
+            self.affichage = "".join(self.affichage)
+
+        elif self.direction == "N":
+            self.affichage = ["|", v2, "|", v1, "|"]
+            self.affichage = "".join(self.affichage)
 
 

b232f168f15fab7b0e4a9cde7c7167cfa2b41452
florent <flobirmbout@outlook.fr>
2023-10-25 09:31:37 +0200

    classes domino et logic

---
+++ Domino.py
@@ -0,0 +1,19 @@
+class Domino :
+    direction
+    value
+    def __init__(self,type,):
+        self.value = [type[0],type[1]]
+
+    def setValue(self,value):
+        self.value=value
+
+    def set_direction(self,dir,):
+        self.direction=dir
+
+    def getValue(self,value):
+        self.value=value
+
+    def getDirection(self,direction):
+        self.direction = direction
+
+

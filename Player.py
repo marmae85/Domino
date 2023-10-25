3038acaf880d6119f15d179fa33bc1f764134934
Marius <marius.humeau@gmail.com>
2023-10-25 10:12:47 +0200

    Player useDomino créée

--- Player.py
+++ Player.py
@@ -1,20 +1,24 @@
 class Player:
-    def __init__(self, game):
-        self.game = game
+    def __init__(self, hand):
+        self.hand = hand
 
     def getMin(self):
         min = 13
-        for i in range(len(self.game)):
-            (v1, v2) = self.game[i]#.getValue()
+        for i in range(len(self.hand)):
+            (v1, v2) = self.hand[i]#.getValue()
             currentvalue = v1 + v2
             if currentvalue < min:
                 min = currentvalue
         return min
     def getMax(self):
         max = 0
-        for i in range(len(self.game)):
-            (v1, v2) = self.game[i]#.getValue()
+        for i in range(len(self.hand)):
+            (v1, v2) = self.hand[i]#.getValue()
             currentvalue = v1 +v2
             if currentvalue > max:
                 max = currentvalue
         return max
+    def useDomino(self, value):
+        for i in range(len(self.hand)):
+            if self.hand.getValue() == value:
+                self.hand.pop(i)

f5a4c03a86c05ac9980ecc582de86cf56fc51e63
flav la menace <flavien.balan@reseau.eseo.fr>
2023-10-25 09:52:51 +0200

    fonction sac dominos construction fonction distribution des dominos

--- main.py
+++ main.py
@@ -2,9 +2,23 @@
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+def bag_dominos():
+    list = []
+    for i in range(7):
+        for j in range(i + 1):
+            list.append((j, i))
+    return list
+bag = bag_dominos() # variable bag est la liste des dominos
+#print(bag)
 
+def distribution():
+    for i in range(8):
+        random_domino = random.randint(0, len(bag) - 1) #pioche un domino random dans le sac
+        main = bag.append()
+    print("voilà tes 7 dominos batard",main)
 
-
+distribution()
 
 def main():
     return
@@ -12,14 +26,8 @@ def main():
 if __name__ == '__main__':
     main()
 
-    def bag_dominos():
-        dominosbag=[]
-        for i in range(7):
-            for j in range(i+1):
-                dominosbag.append((j,i))
-        print(dominosbag)
-    bag_dominos()
-    print("patate")
+
+
 
 
 

f72e32eb6b938f6383754a319989bdff94181d14
Marius <marius.humeau@gmail.com>
2023-10-26 14:05:12 +0200

    display rules créée

--- Display.py
+++ Display.py
@@ -1,4 +1,9 @@
+def screen_clear():
+    for i in range(100):
+        print("\n")
+
 def displayMenu():
+    screen_clear()
     toReturn = ""
     print("DOMINO")
     print("1. Jouer")
@@ -16,4 +21,14 @@ def displayMenu():
             print("La valeur saisie ne fonctionne pas")
     return toReturn
 
-print(displayMenu())
+def displayRules():
+    screen_clear()
+    print(f"Les dominos est un jeu dans lequel plusieurs joueurs s'affrontent. Le but est de terminer sa main le plus rapidement possible")
+    print(f"\n\nAppuyez sur n'importe quelle touche pour revenir au menu...")
+    input()
+    displayMenu()
+
+
+choice = displayMenu()
+if choice == 3:
+    displayRules()

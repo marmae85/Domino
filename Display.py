8f40a9dfb7f2c6790f8b19b5ebb2fe0ea7fd6b37
Marius <marius.humeau@gmail.com>
2023-10-26 14:47:24 +0200

    position et clr fonctionnels

--- Display.py
+++ Display.py
@@ -1,34 +1,40 @@
+def clr():
+    print("\033[2J")
+def pos(x,y):
+    print("\033["+str(y)+";"+str(x)+"H", end="")
+def printPos(x, y, text):
+    print("\033["+str(y)+";"+str(x)+"H"+text)
+
 def screen_clear():
     for i in range(100):
         print("\n")
 
 def displayMenu():
-    screen_clear()
-    toReturn = ""
-    print("DOMINO")
-    print("1. Jouer")
-    print("2. Charger une partie")
-    print("3. Règles")
-    print("4. Quitter")
+    error = False
     while 1:
         try:
+            clr()
+            if error:
+                pos(25,12)
+                print("La valeur saisie ne fonctionne pas")
+            toReturn = ""
+            printPos(50, 2,"DOMINO")
+            printPos(30,4,"1. Jouer")
+            printPos(30,5,"2. Charger une partie")
+            printPos(30,6,"3. Règles")
+            printPos(30,7,"4. Quitter")
+            pos(25,10)
             toReturn = int(input("Que voulez-vous faire ? : "))
             if 0 < toReturn <= 4:
                 break
             else:
-                print("La valeur saisie n'est pas dans une option")
+                error = True
         except Exception as e:
-            print("La valeur saisie ne fonctionne pas")
+            error = True
     return toReturn
 
 def displayRules():
-    screen_clear()
+    clr()
     print(f"Les dominos est un jeu dans lequel plusieurs joueurs s'affrontent. Le but est de terminer sa main le plus rapidement possible")
     print(f"\n\nAppuyez sur n'importe quelle touche pour revenir au menu...")
     input()
-    displayMenu()
-
-
-choice = displayMenu()
-if choice == 3:
-    displayRules()

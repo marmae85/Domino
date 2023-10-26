2359cc0cc441dbd02d7ec6256d05fb33212de2bf
Marius <marius.humeau@gmail.com>
2023-10-26 16:39:23 +0200

    main avec boucle de fond

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
-# This is a sample Python script.
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import random
-from logic import jeu
-#from Display import displayMenu
+from logic import init, partie
+from Display import displayMenu, displayRules, clr
 def main():
-    name1 = str(input("quel est le nom du joueur 1 ?"))
-    name2 = str(input("quel est le nom du joueur 2 ?"))
-    init(name1, name2)
-    """choix=displayMenu()
-    if choix==1:
-        name1=str(input("quel est le nom du joueur 1?)
-        name2=str(input("quel est le nom du joueur 2?)
-        jeu(name1,name2)
-    elif choix== 2 :
-        "charger"
-    elif choix == 3:
-        "regles"
-    elif choix==4:
-        "quitter"
-    """
+    while 1:
+        action = displayMenu()
+        if action == 1:
+            name1 = input("Entrez le nom d'un joueur : ")
+            name2 = input("Entrez le nom d'un joueur : ")
+            init(name1, name2)
+            partie()
+        elif action == 2:
+            loadGame()
+        elif action == 3:
+            displayRules()
+        else:
+            clr()
+            exit()
     return
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-
-
-
 
 
 

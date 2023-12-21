5b58c5724e7ef84e4db53df65141eba1eb683bb7
Marius <marius.humeau@gmail.com>
2023-12-21 14:17:35 +0100

    inversion de l'affichage dans des dominos

--- Display.py
+++ Display.py
@@ -1,3 +1,5 @@
+import os
+
 from Domino import Domino
 def clr():
     print("\033[2J")
@@ -7,8 +9,9 @@ def printPos(x, y, text):
     print("\033["+str(y)+";"+str(x)+"H"+text)
 
 def screen_clear():
-    for i in range(100):
-        print("\n")
+    #for i in range(100):
+    #    print("\n")
+    os.system("clear")
 
 def displayMenu():
     error = False
@@ -18,7 +21,6 @@ def displayMenu():
             if error:
                 pos(25,12)
                 print("La valeur saisie ne fonctionne pas")
-            toReturn = ""
             printPos(50, 2,"DOMINO")
             printPos(30,4,"1. Jouer")
             printPos(30,5,"2. Charger une partie")
@@ -35,9 +37,11 @@ def displayMenu():
     return toReturn
 
 def displayBoard(boardList):
+    #clr()
+    #printPos(80,40,str(len(boardList)))
     for i in range(len(boardList)):
         (v1, v2) = boardList[i].getValue()
-        if boardList[i].getDirection() == "W":
+        if boardList[i].getDirection() == "E":
             printPos(10+(6*i), 30,"["+str(v2) +"|"+str(v1)+"]")
         else:
             printPos(10+(6*i), 30,"["+str(v1) +"|"+str(v2)+"]")
@@ -47,8 +51,8 @@ def displayHand(hand):
     for i in range(len(hand)):
         (v1, v2) = hand[i].getValue()
         esp = int(50/len(hand))
-        print(esp)
-        printPos(50 + esp*i, 30, "[" + str(v2) + "|" + str(v1) + "]")
+        #print(esp)
+        printPos(50 + esp*i, 30-1, "[" + str(v2) + "|" + str(v1) + "]")
 
 def displayRules():
     clr()
@@ -56,13 +60,3 @@ def displayRules():
     print(f"\n\nAppuyez sur n'importe quelle touche pour revenir au menu...")
     input()
 
-
-"""d0 = Domino((3, 4))
-d1 = Domino((6, 3))
-d2 = Domino((0, 4))
-d5 = Domino((3, 3))
-d3 = Domino((1, 5))
-d0.setDirection("W")
-clr()
-board = [d0,d1,d2,d3,d5]
-displayHand(board)"""
